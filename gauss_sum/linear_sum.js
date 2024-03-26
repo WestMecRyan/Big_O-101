918bb1e69a525e402c12b3f00d192f4b087fd540
Ryan Morales <ryan.morales@west-mec.org>
2024-03-26 21:30:35 +0000

    finished 12pm

---
+++ gauss_sum/linear_sum.js
@@ -0,0 +1,9 @@
+function linearSum(n) { 
+    let sum = 0;
+    for (let i = 1; i <= n; i++) { 
+        // sum = sum + i;
+        sum += i;
+    }
+    return sum;
+}
+console.log(linearSum(100));
