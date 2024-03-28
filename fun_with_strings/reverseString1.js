08e9b04514ce3bc65259f71a91be1450a99fec72
Ryan Morales <ryan.morales@west-mec.org>
2024-03-28 00:25:51 +0000

    end of reverse string lesson

--- fun_with_strings/reverseString1.js
+++ fun_with_strings/reverseString1.js
@@ -1,9 +1,12 @@
 // initialize a variable called someStr to some string of your choice
-
+let someStr = "Hello World"; 
 // create a named function that takes an argument `str`
+function reverseString(str) {
+   // reverse str 
+    str = str.split("").reverse().join("");
+    return str; 
+ }
 
-// in the function reverse `str`
-
-// return `str`
 
 // outside of the function log the function and pass someStr
+console.log(reverseString(someStr));
