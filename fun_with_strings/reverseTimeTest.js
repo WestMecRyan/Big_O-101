046c1cfb47d308c0a0dc0c5e217ebeb74403cea4
Ryan Morales <ryan.morales@west-mec.org>
2024-03-28 20:49:33 +0000

    completed revStrConcat

--- fun_with_strings/reverseTimeTest.js
+++ fun_with_strings/reverseTimeTest.js
@@ -2,6 +2,8 @@ let someStr = "Hello World";
 function revStrConcat(str) { 
     let revStr = ""; 
     for (let i = str.length - 1; i >= 0; i--) { 
-        
+        revStr += str[i];
     }
+    return revStr;
 }
+console.log(revStrConcat(someStr));
