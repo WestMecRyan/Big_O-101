4b0abce3468ea731fce943ce2cac21213e5eb09b
Ryan Morales <ryan.morales@west-mec.org>
2024-03-28 21:08:07 +0000

    2 time tests

--- fun_with_strings/reverseTimeTest.js
+++ fun_with_strings/reverseTimeTest.js
@@ -9,14 +9,20 @@ function revStrConcat(str) {
 }
 function buildStr(n) { 
     let str = "";
-    for (let i = 0; i <= n; i++) { 
-        str += i;
+    for (let i = 1; i <= n; i++) { 
+        str += "a";
     }
     return str;
 }
 charCount = 10;
 someStr = buildStr(charCount);
-console.time('revStrConcat test');
+console.time(`revStrConcat test ${charCount}`);
 revStrConcat(someStr);
-console.timeEnd('revStrConcat test');
+console.timeEnd(`revStrConcat test ${charCount}`);
+// console.log(revStrConcat(someStr));
+charCount = 100;
+someStr = buildStr(charCount);
+console.time(`revStrConcat test ${charCount}`);
+revStrConcat(someStr);
+console.timeEnd(`revStrConcat test ${charCount}`);
 // console.log(revStrConcat(someStr));
